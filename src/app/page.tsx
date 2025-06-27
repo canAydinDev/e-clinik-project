43e2748056896d8bbb6032c98105cd99763ba156
canaydindev <canaydindev@gmail.com>
2025-06-27 23:55:40 +0300

    1: update-size

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,9 @@ export default function Home() {
   return (
     <div>
       <h1>Hello</h1>
-      <Button variant="elevated">Click Me</Button>
+      <Button variant="elevated" size="lg">
+        Click Me
+      </Button>
     </div>
   );
 }
