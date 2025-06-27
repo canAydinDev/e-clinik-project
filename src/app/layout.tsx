7658974b68800e163e9d8b10cbcbde2e601a17cd
canaydindev <canaydindev@gmail.com>
2025-06-27 23:53:50 +0300

    customization

---
+++ src/app/layout.tsx
@@ -0,0 +1,24 @@
+import type { Metadata } from "next";
+import { DM_Sans } from "next/font/google";
+import "./globals.css";
+
+const dmSans = DM_Sans({
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "E-Clinic",
+  description: "My Last E Clinic Web Site",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${dmSans.className} $ antialiased`}>{children}</body>
+    </html>
+  );
+}
