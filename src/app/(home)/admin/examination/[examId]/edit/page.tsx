3a7af2314825dcef1f55f322f8168cd679d355e8
canaydindev <canaydindev@gmail.com>
2025-08-13 00:01:13 +0300

    v06

--- src/app/(home)/admin/examination/[examId]/edit/page.tsx
+++ src/app/(home)/admin/examination/[examId]/edit/page.tsx
@@ -1,13 +1,12 @@
-// src/app/(home)/admin/examination/[examId]/edit/page.tsx
 import { getExaminationByExamId } from "@/lib/actions/examinations.actions";
 import { EditExaminationForm } from "@/modules/ui/components/admin-page/examination-page/update-examination-form";
 
 interface PageProps {
-  params: { examId: string }; // <-- Promise değil
+  params: Promise<{ examId: string }>;
 }
 
 const Page = async ({ params }: PageProps) => {
-  const { examId } = params; // <-- await yok
+  const { examId } = await params;
   const exam = await getExaminationByExamId(examId);
 
   if (!exam) return <div>Muayene bulunamadı.</div>;
